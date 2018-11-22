a0aff6d9a354e5f9f133c922c41224795904002f
hbz2207 <43958147+hbz2207@users.noreply.github.com>
2018-11-22 11:46:09 +0100

    update scenes
    scenes en controllers toegevoegd op basis van lokale data

---
+++ testProjecten/gui/Menu/Assets/Scenes/NavigateButtonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NavigateButtonController : MonoBehaviour {
+
+    
+    public void navigate()
+    {
+        //print kan ook gebruikt worden
+        Debug.Log("navigate");
+    }
+
+
+
+//overige functies mogen er bij
+
+}
---
+++ testProjecten/gui/Menu/Assets/Scenes/SettingsButtonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SettingsButtonController : MonoBehaviour {
+
+   
+    public void goToSettings(string sceneNaam){
+        //functie print kan ook gebruikt worden ipv debug.log
+        //bron: https://answers.unity.com/questions/1369393/how-to-switch-between-scenes.html
+        SceneManager.LoadScene(sceneNaam);
+
+	Debug.Log("ga naar settings");
+}
+
+//overige functies mogen er bij
+
+}
