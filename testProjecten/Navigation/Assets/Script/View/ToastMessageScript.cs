7a7974c9a2209fe5e9164cb712e664905f32eb38
jensdesmeyter <43971861+jensdesmeyter@users.noreply.github.com>
2018-12-12 22:57:22 +0100

    Navigation with image markers

---
+++ testProjecten/Navigation/Assets/Script/View/ToastMessageScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class ToastMessageScript : MonoBehaviour
+{
+    string toastString;
+    string input;
+    AndroidJavaObject currentActivity;
+    AndroidJavaClass UnityPlayer;
+    AndroidJavaObject context;
+
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+        }
+    }
+    
+
+    public void showToastOnUiThread(string toastString)
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            this.toastString = toastString;
+            currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(showToast));
+        }
+    }
+
+    void showToast()
+    {
+        Debug.Log(this + ": Running on UI thread");
+
+        AndroidJavaClass Toast = new AndroidJavaClass("android.widget.Toast");
+        AndroidJavaObject javaString = new AndroidJavaObject("java.lang.String", toastString);
+        AndroidJavaObject toast = Toast.CallStatic<AndroidJavaObject>("makeText", context, javaString, Toast.GetStatic<int>("LENGTH_SHORT"));
+        toast.Call("show");
+    }
+    
+
+    public void showToastOnUiThread(string toastString, bool isShort)
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            this.toastString = toastString;
+            if(isShort){currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(showShortToast));}
+            else{currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(showLongToast));}
+        }
+    }
+
+    void showShortToast()
+    {
+        Debug.Log(this + ": Running on UI thread");
+
+        AndroidJavaClass Toast = new AndroidJavaClass("android.widget.Toast");
+        AndroidJavaObject javaString = new AndroidJavaObject("java.lang.String", toastString);
+        AndroidJavaObject toast = Toast.CallStatic<AndroidJavaObject>("makeText", context, javaString, Toast.GetStatic<int>("LENGTH_SHORT"));
+        toast.Call("show");
+    }
+
+    void showLongToast()
+    {
+        Debug.Log(this + ": Running on UI thread");
+
+        AndroidJavaClass Toast = new AndroidJavaClass("android.widget.Toast");
+        AndroidJavaObject javaString = new AndroidJavaObject("java.lang.String", toastString);
+        AndroidJavaObject toast = Toast.CallStatic<AndroidJavaObject>("makeText", context, javaString, Toast.GetStatic<int>("LENGTH_LONG"));
+        toast.Call("show");
+    }
+}
