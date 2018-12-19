97a98f689c6e8eec4eecdfffd3d27724c558a485
hbz2207 <43958147+hbz2207@users.noreply.github.com>
2018-12-19 11:39:17 +0100

    change in icalScene and added icalcontroller
    Changed label on icalScene and added ical controller with open desiderius function

---
+++ testProjecten/gui/Menu/Assets/Scenes/IcalController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcalController : MonoBehaviour {
+    //this function will open your personal timetable on desiderius
+	public void OpenDesiderius () {
+        Application.OpenURL("https://desiderius.ehb.be/index.php?application=Ehb%5CApplication%5CCalendar%5CExtension%5CSyllabusPlus&syllabus_action=UserBrowser");
+    }
+
+
+//this function will open the calender on the device 	
+	public void OpenCalender () {
+		
+	}
+}
