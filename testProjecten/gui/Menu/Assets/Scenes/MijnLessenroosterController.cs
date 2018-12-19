5fe99b0b2835a077d3c77a3574c4583c2fbcaa02
hbz2207 <43958147+hbz2207@users.noreply.github.com>
2018-12-20 00:59:37 +0100

    changed scripts
    changed scripts of some scenes

--- testProjecten/gui/Menu/Assets/Scenes/MijnLessenroosterController.cs
+++ testProjecten/gui/Menu/Assets/Scenes/MijnLessenroosterController.cs
@@ -5,17 +5,10 @@ using UnityEngine;
 public class MijnLessenroosterButtonController : MonoBehaviour {
 
 
-/*public IEnumerator ShowLessenrooster(){
-        // aan de hand van webscraping lessen rooster tonen 
-        // bron: https://docs.unity3d.com/ScriptReference/WWW.html
-        WWW www = new WWW(url);
-        Renderer renderer = GetComponent<Renderer>();
-        renderer.material.mainTexture = www.texture;
-        yield return www;
-       print(www);
-    }*/
+ public void goToLessenRooster()
+    {
+        SceneManager.LoadScene("OpenIcalScene");
+    }
 
-
-//overige functies mogen er bij
 
 }
