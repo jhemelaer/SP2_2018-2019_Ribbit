048334386a7f67d840602aceb3cf83160f8e1b2b
jensdesmeyter <43971861+jensdesmeyter@users.noreply.github.com>
2018-12-19 22:55:29 +0100

    navigation  finished +-

---
+++ testProjecten/NavigationRibbit/Assets/Script/ArrowScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour //handels the transformation of the arrow
+{
+
+    MarkerData targetData;
+    private const int markerXRotation = 270;
+
+    // Use this for initialization
+    void Start()
+    {
+        targetData = transform.parent.gameObject.GetComponent<MarkerData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /* get world coordinate, rotate to input */
+    public void RotateArrow(Vector3 eulerRotation)
+    {
+        transform.rotation = transform.parent.rotation;
+        transform.Rotate(eulerRotation);
+        Debug.Log(transform.rotation.eulerAngles + " " + (eulerRotation.y - transform.rotation.y) + " " + eulerRotation.y);
+    }
+
+    public void PointArrowUp()
+    {
+        transform.rotation = transform.parent.rotation;
+        transform.Rotate(new Vector3(90, 0, 0));
+    }
+    public void PointArrowDown()
+    {
+        transform.rotation = transform.parent.rotation;
+        transform.Rotate(new Vector3(270, 0, 0));
+    }
+
+    public void PointToZero()
+    {
+        transform.rotation = transform.parent.rotation;
+        Debug.Log("To Zero " + transform.rotation.eulerAngles);
+    }
+
+    public void PointToCoordinate(Vector3 destination) /* Get Destination of object to point, return degree and rotate arrow */
+    {
+        targetData = transform.parent.gameObject.GetComponent<MarkerData>();
+        float planeDistance = Vector2.Distance(targetData.position, destination);
+        float deltaX = targetData.position.x - destination.x;
+        float deltaY = targetData.position.z - destination.z;
+
+        transform.Rotate(0, 0, (((Mathf.Atan2(deltaY, -deltaX)) * 180 / Mathf.PI) + markerXRotation) + (360 - targetData.orientation.y));
+        Debug.Log("-- " + targetData.position + " " + destination);
+        Debug.Log("-- Point ToCoorDinate -- " + deltaY + " " + deltaX + " " + (((Mathf.Atan2(deltaY, -deltaX)) * 180 / Mathf.PI) + 270));
+    }
+
+    public void PrintSmt(string p)
+    {
+        Debug.Log(p);
+    }
+}
---
+++ testProjecten/NavigationRibbit/Assets/Script/View/ShowStateManager.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowStateManager : MonoBehaviour {
+
+	public GameObject actionBar;
+	Image actionBarImg;
+	Text appNameText;
+	private ToastMessageScript toastMessageScript;
+	private enum StateColor
+	{
+		Idle,
+		Navigate
+	}
+
+	// Use this for initialization
+	void Start () {
+		toastMessageScript = gameObject.GetComponent<ToastMessageScript>();
+		actionBarImg = actionBar.GetComponent<Image>();
+		appNameText = actionBar.gameObject.transform.Find("AppName").gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//recive from MainController, may be implement from callback
+	public void OnBeginPointChange(GameObject bpoint)// shows message to user when BeginPoint is found
+    {
+        GameObject dpoint = MainController.instance.destinationPoint;
+        GameObject rpoint = MainController.instance.reachedPoint;
+		string dp = "null";
+        string rp = "null";
+		string mesge = "---";
+        if (dpoint == null && rpoint == null)
+        {
+            mesge = "You are at: " + bpoint.GetComponent<MarkerData>().roomName + " You are now able to select a destination.";
+            ChangeActionText("at:" + bpoint.GetComponent<MarkerData>().roomName);
+        }
+        else if (dpoint != null && rpoint == null)
+        {
+            dp = dpoint.GetComponent<MarkerData>().roomName;
+            mesge = "You are now at: " + bpoint.GetComponent<MarkerData>().roomName;
+            ChangeActionText("Go to:" + dpoint.GetComponent<MarkerData>().roomName);
+
+        }
+        else if (dpoint == null && rpoint != null)
+        {
+            rp = rpoint.GetComponent<MarkerData>().roomName;
+            if (bpoint.GetComponent<MarkerData>().roomName == rpoint.GetComponent<MarkerData>().roomName)
+            {
+                mesge = "Arrived at the destination " + bpoint.GetComponent<MarkerData>().roomName;
+                ChangeActionText("Arrived:" + bpoint.GetComponent<MarkerData>().roomName);
+            }
+            else
+            {
+                mesge = "You are now at: " + bpoint.GetComponent<MarkerData>().roomName;
+                ChangeActionText("at:" + bpoint.GetComponent<MarkerData>().roomName);
+            }
+        }
+        else if (dpoint != null && rpoint != null)
+        {
+            dp = dpoint.GetComponent<MarkerData>().roomName;
+            rp = rpoint.GetComponent<MarkerData>().roomName;
+            if (bpoint.GetComponent<MarkerData>().roomName == rpoint.GetComponent<MarkerData>().roomName)
+            {
+                mesge = "You are now at: " + bpoint.GetComponent<MarkerData>().roomName;
+                ChangeActionText("Go to:" + dpoint.GetComponent<MarkerData>().roomName);
+            }
+            else
+            {
+                mesge = "You are now at: " + bpoint.GetComponent<MarkerData>().roomName;
+                ChangeActionText("at:" + dpoint.GetComponent<MarkerData>().roomName);
+            }
+        }
+        Debug.Log(mesge + "  |  Begin Change - State:" + MainController.instance.appState + "  |  "
+        + "dest: " + dp + "  reach: " + rp);
+        toastMessageScript.showToastOnUiThread(mesge, true);
+        ChangeActionBarColor(MainController.instance.appState == MainController.AppState.Idle ? StateColor.Idle : StateColor.Navigate);
+    }
+
+    public void OnDestinationPointChange(GameObject dpoint)// shows message to user when destinationPoint is chosen
+    {
+        GameObject bpoint = MainController.instance.beginPoint;
+        GameObject rpoint = MainController.instance.reachedPoint;
+		string bp = "null";
+        string rp = "null";
+		string mesge = "---";
+        if (dpoint == null)
+        {
+            mesge = "Navigation canceled";
+            ChangeActionText("Ribbit");
+        }
+        else if (bpoint == null && dpoint != null && rpoint == null)
+        {
+            mesge = "Chosen destination: " + dpoint.GetComponent<MarkerData>().roomName + "\n Please walk around To identify your location";
+            ChangeActionText("Searching for location");
+            toastMessageScript.showToastOnUiThread(mesge, true);
+        }
+        else if (bpoint != null && dpoint != null && rpoint == null)
+        {
+            bp = bpoint.GetComponent<MarkerData>().roomName;
+            mesge = "Start navigating to: " + dpoint.GetComponent<MarkerData>().roomName + " already";
+            ChangeActionText("Go to:" + dpoint.GetComponent<MarkerData>().roomName);
+        }
+        else if (bpoint == null && dpoint != null && rpoint != null)
+        {
+            rp = rpoint.GetComponent<MarkerData>().roomName;
+            mesge = "Start navigating to?" + dpoint.GetComponent<MarkerData>().roomName + " already";
+            ChangeActionText("Still:" + dpoint.GetComponent<MarkerData>().roomName);
+
+        }
+        else if (bpoint != null && dpoint != null && rpoint != null)
+        {
+            bp = bpoint.GetComponent<MarkerData>().roomName;
+            rp = rpoint.GetComponent<MarkerData>().roomName;
+            if (bpoint.GetComponent<MarkerData>().roomName == dpoint.GetComponent<MarkerData>().roomName)
+            {
+                mesge = "You are already at your destination. Please chose an other destination: " + dpoint.GetComponent<MarkerData>().roomName;
+            }
+            else if (dpoint.GetComponent<MarkerData>().roomName == rpoint.GetComponent<MarkerData>().roomName)
+            {
+                mesge = "Start navigating to: " + dpoint.GetComponent<MarkerData>().roomName + "already";
+                ChangeActionText("Go to:" + dpoint.GetComponent<MarkerData>().roomName);
+            }
+            else
+            {
+                mesge = "Start navigating to: " + dpoint.GetComponent<MarkerData>().roomName + " already";
+                ChangeActionText("Go to:" + dpoint.GetComponent<MarkerData>().roomName);
+            }
+        }
+        Debug.Log(mesge + "  |  Dest Change - State:" + MainController.instance.appState + "  |  "
+        + "begin: " + bp + "  reach: " + rp);
+        toastMessageScript.showToastOnUiThread(mesge, true);
+        ChangeActionBarColor(MainController.instance.appState == MainController.AppState.Idle ? StateColor.Idle : StateColor.Navigate);
+    }
+
+    private void ChangeActionBarColor(StateColor color)
+	{
+		switch (color)
+        {
+            case StateColor.Idle:
+                actionBar.GetComponent<Image>().color = new Color32(60, 126, 255, 255);
+                break;
+            case StateColor.Navigate:
+                actionBar.GetComponent<Image>().color = new Color32(126, 60, 255, 255);
+                break;
+            default: actionBar.GetComponent<Image>().color = new Color32(60, 126, 255, 255); break;
+        }
+	}
+
+	private void ChangeActionText(string actext)
+    {
+        appNameText.text = actext;
+    }
+}
